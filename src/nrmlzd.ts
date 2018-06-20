1ea36385d483cd7973ad8773e47ad1c89d99ae15
Alex Freska <alex.freska@gmail.com>
2018-06-20 18:08:12 -0400

    Nrmlzd data renamed to entities

--- src/nrmlzd.ts
+++ src/nrmlzd.ts
@@ -3,76 +3,76 @@ import { isPlainObject, omit, union, without } from 'lodash'
 export type Id = string
 export type Ids = string[]
 
-export interface Item {
+export interface IEntity {
   id: Id
 }
 
-export interface IData {
-  [key: string]: Item
+export interface IEntityMap {
+  [key: string]: IEntity
 }
 
 export interface INorm {
   ids: Ids,
-  data: IData
+  entities: IEntityMap
 }
 
 export default {
   create: (): INorm => ({
-    data: {},
+    entities: {},
     ids: [],
   }),
-  fromArray: (array: Item[]) =>
+  fromArray: (array: IEntity[]) =>
     array.reduce(
-      (acc: INorm, entity: Item) => ({
-        data: {
-          ...acc.data,
+      (acc: INorm, entity: IEntity) => ({
+        entities: {
+          ...acc.entities,
           [entity.id]: entity
         },
         ids: union(acc.ids, [entity.id]),
       }),
       {
-        data: {},
+        entities: {},
         ids: [],
       }
     ),
   pickFrom: (norm: INorm, ids: Ids) => ({
-    data: mapPickIds(ids, norm.data),
+    entities: mapPickIds(ids, norm.entities),
     ids,
   }),
   remove: (norm: INorm, entityIdOrIds: Id|Ids): INorm => ({
-    data: omit(norm.data, entityIdOrIds) as IData,
+    entities: omit(norm.entities, entityIdOrIds) as IEntityMap,
     ids: without(norm.ids, entityIdOrIds) as Ids,
   }),
-  toArray: (arg1: Ids|INorm, arg2?: IData) => {
-    const { ids, data } = (arg2 ? { ids: arg1, data: arg2 } : arg1 as INorm)
-    if (!ids || !data) {
+  toArray: (arg1: Ids|INorm, arg2?: IEntityMap) => {
+    const { ids, entities } = (arg2 ? { ids: arg1, entities: arg2 } : arg1 as INorm)
+    if (!ids || !entities) {
       console.warn('Normalized.toArray: argument error') // tslint:disable-line
       return []
     }
-    return (ids as Ids).map(id => data[id]) as Item[]
+    return (ids as Ids).map(id => entities[id]) as IEntity[]
   },
-  upsert: (norm: INorm, entityOrEntities: Item|Item[], { merge }: { merge?: boolean } = {}) => {
+  upsert: (norm: INorm, entityOrEntities: IEntity|IEntity[], { merge }: { merge?: boolean } = {}) => {
     const isSingle = isPlainObject(entityOrEntities)
 
     if (isSingle) {
-      const entity = entityOrEntities as Item
+      const entity = entityOrEntities as IEntity
 
       return {
-        data: {
-          ...norm.data,
+        entities: {
+          ...norm.entities,
           [entity.id]: merge ? mergeIntoExisting(norm, entity) : entity
         },
         ids: union(norm.ids, [entity.id]),
       }
     }
 
-    const entities: Item[] = merge
-      ? (entityOrEntities as Item[]).map((entity: Item) => mergeIntoExisting(norm, entity))
-      : entityOrEntities as Item[]
+    const entities: IEntity[] = merge
+      ? (entityOrEntities as IEntity[]).map((entity: IEntity) => mergeIntoExisting(norm, entity))
+      : entityOrEntities as IEntity[]
 
     return {
-      data: {
-        ...norm.data,
+      entities: {
+        ...norm.entities,
         ...arrayToMap(entities)
       },
       ids: union(norm.ids, entities.map(({ id }) => id)),
@@ -80,10 +80,10 @@ export default {
   },
 }
 
-const mergeIntoExisting = (norm: INorm, item: Item) =>
-  Object.assign({}, norm.data[item.id] || {}, item)
+const mergeIntoExisting = (norm: INorm, item: IEntity) =>
+  Object.assign({}, norm.entities[item.id] || {}, item)
 
-const mapPickIds = (ids: Ids, map: IData) =>
+const mapPickIds = (ids: Ids, map: IEntityMap) =>
   ids.reduce(
     (acc, id) => ({
       ...acc,
@@ -92,7 +92,7 @@ const mapPickIds = (ids: Ids, map: IData) =>
     {}
   )
 
-const arrayToMap = (array: Item[]) =>
+const arrayToMap = (array: IEntity[]) =>
   array.reduce(
     (acc, entity) => ({
       ...acc,
